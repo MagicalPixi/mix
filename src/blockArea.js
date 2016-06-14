95c05ed75b7260168ae8f72fc1135ed8360840de
guoshencheng <648772021@qq.com>
2016-06-14 15:49:20 +0800

    clear code

--- src/blockArea.js
+++ src/blockArea.js
@@ -3,43 +3,31 @@
  */
 var blockAreaCul = function(leftData, rightData) {
     var blockAreas = [];
-    var before = 0;
-    var i = 0;
-    var j = 0;
-    var leftStart = 0;
-    var rightStart = 0;
+    var before, i, j, leftStart, rightStart = 0;
     while(i < leftData.length && j < rightData.length) {
         var curLeft = leftData[i];
         var curRight = rightData[j];
-        var remainLeft = curLeft.count-leftStart;
-        var remainRight = curRight.count-rightStart;
+        var remainLeft = curLeft.count - leftStart;
+        var remainRight = curRight.count - rightStart;
         var currentHeight;
-        if(remainLeft > remainRight) {
-            currentHeight = remainRight;
-            leftStart = leftStart+remainRight;
-            rightStart = 0;
-            j++;
-        } else if(remainLeft == remainRight) {
-            currentHeight = remainLeft;
-            leftStart = 0;
-            rightStart = 0;
-            i++;
-            j++;
+        if (remainLeft == remainRight) {
+          currentHeight = remainLeft
+          leftStart = rightStart = 0
+          i++
+          j++
         } else {
-            currentHeight = remainLeft;
-            leftStart = 0;
-            rightStart = rightStart+remainLeft;
-            i++;
+          currentHeight = remainLeft > remainRight ? remainRight : remainLeft
+          remainLeft > remainRight ? leftStart += currentHeight : rightStart += currentHeight
+          remainLeft > remainRight ? j++ : i++
         }
         var curArea = {
-            before : before*80,
-            height : currentHeight*80,
-            isEffect : curLeft.isRed != curRight.isRed
+            before : before,
+            height : currentHeight,
+            vaild : curLeft.isRed != curRight.isRed
         };
         blockAreas.push(curArea);
         before += currentHeight;
     }
     return blockAreas;
 }
-
 module.exports = blockAreaCul;
